02f46ee438cdaec31dd5baf2bbf087b556d1c72f
Dries Geenen <driesgeenen@gmail.com>
2017-12-12 14:58:43 +0100

    Backend fix

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String
-    },
     username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
         type: String,
         unique: true,
         required: true
