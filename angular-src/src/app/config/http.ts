0bd4b7a91ef0b43a97a0a4c3f2ce63908e451215
Rik Helsen <rik_helsen@hotmail.com>
2017-12-14 14:14:55 +0100

    Login fixed :D

--- angular-src/src/app/config/http.ts
+++ angular-src/src/app/config/http.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export default class Config{
-	static host:string = "http://localhost:3000";
+	static host:string = "http://localhost:6600";
 
 	static headers:HttpHeaders = new HttpHeaders().set('content-type', 'application/json').set("Authorization", localStorage.getItem("authToken"));
 
