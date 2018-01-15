4170c85c812c7e1bab2f714e4d653f3123ca18a9
Dries Geenen <driesgeenen@gmail.com>
2018-01-15 15:40:47 +0100

    Final

--- angular-src/src/app/config/http.ts
+++ angular-src/src/app/config/http.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export default class Config{
-	static host:string = "http://localhost:6600";
+	static host:string = "http://fonologo.hopto.org:6600";
 
 	static login_headers:HttpHeaders = new HttpHeaders().set('content-type', 'application/json');
 	static headers:HttpHeaders = new HttpHeaders().set('content-type', 'application/json').set("Authorization", localStorage.getItem("authToken"));
